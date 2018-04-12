b521974dd631f848b0392ad027d3b136833e31e9
xxbingru <xxbingru@gmail.com>
2018-04-12 19:08:23 +0200

    faker valeur aleatoire

--- inscrit.php
+++ inscrit.php
@@ -1,29 +1,51 @@
 <?php
 	
 	include "database.php";
-	
-	
-	if(isset($_POST["motdepass"]) && isset($_POST["mel"]) && isset($_POST["nom"]) && isset($_POST["prenom"]))
-	{
+	include "Faker/src/autoload.php";
 
-		
+	$faker = Faker\Factory::create('fr_FR');
+	
+	
+	
+	
+	if(isset($_POST["motdepass"]) && isset($_POST["mel"]) && isset($_POST["nom"]) && isset($_POST["prenom"])){
+		$mail = htmlspecialchars($_POST["mel"]);
+		$mdp = htmlspecialchars($_POST["motdepass"]);
+		$mdp = sha1($mdp);
+		$nom = htmlspecialchars($_POST["nom"]);
+		$prenom = htmlspecialchars($_POST["prenom"]);
+
 		$abonne=$bdd->prepare("INSERT INTO User(mail, motdepasse, nomUser, prenomUser) VALUES (:mail, :motdepasse, :nomUser, :prenomUser)");
-		$abonne->bindValue(':mail',$_POST["mel"],PDO::PARAM_STR); 
-		$abonne->bindValue(':motdepasse',$_POST["motdepass"], PDO::PARAM_STR); 
-		$abonne->bindValue(':nomUser',$_POST["nom"], PDO::PARAM_STR); 
-		$abonne->bindValue(':prenomUser',$_POST["prenom"], PDO::PARAM_STR); 
+		//creation de personne faker
+		/*for($i=0; $i<40; $i++){
+			$mail = $faker->email;
+			$mdp = sha1($faker->password);
+			$nom = $faker->lastname;
+			$prenom = $faker->firstname;
+
+			$abonne->bindValue(':mail',$mail,PDO::PARAM_STR); 
+			$abonne->bindValue(':motdepasse',$mdp, PDO::PARAM_STR); 
+			$abonne->bindValue(':nomUser',$nom, PDO::PARAM_STR); 
+			$abonne->bindValue(':prenomUser',$prenom, PDO::PARAM_STR);
+			$enregistrer=$abonne->execute();
+		}*/
+
+		// création classique 
+		$abonne->bindValue(':mail',$mail,PDO::PARAM_STR); 
+		$abonne->bindValue(':motdepasse',$mdp, PDO::PARAM_STR); 
+		$abonne->bindValue(':nomUser',$nom, PDO::PARAM_STR); 
+		$abonne->bindValue(':prenomUser',$prenom, PDO::PARAM_STR); 
 
 		$enregistrer=$abonne->execute();
 	}
 		
-		if($enregistrer){
-			$message='Vous êtes bien inscrit!';
-		}
-		else{
-			$message='Echec d\inscription';
-		}
+	if($enregistrer){
+		$message='Vous êtes bien inscrit!';
+	}
+	else{
+		$message='Echec d\inscription';
+	}
 	
-
 ?>
 <!DOCTYPE html>
 <html>
